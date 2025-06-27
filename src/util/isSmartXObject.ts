55f6677b4ac7fcd195bbe88b07e2d982dbdbcf83
Tom Pacheco <tom.pacheco@comfortsystemsusa.com>
2025-06-27 08:19:46 -0400

    added additional checks and comments

--- src/util/isSmartXObject.ts
+++ src/util/isSmartXObject.ts
@@ -1,5 +1,10 @@
 
+// isSmartXObject checks if the given ObjectInfo is a SmartX object.
 export function isSmartXObject(oi: ObjectInfo) {
+
+  if (!oi.properties || !oi.properties.ModelName || !oi.properties.ModelName.value) {
+    return false;
+  }
 
   const modelName = oi.properties.ModelName.value as string;
 
